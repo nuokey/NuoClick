8e48dc24d041fe4cfd6e56c122a096043c79211d
Timo <66961498+nuokey@users.noreply.github.com>
2021-01-02 15:08:27 +0300

    Update

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ import time
 import config
 
 bot = telebot.TeleBot(config.test_token)
+admins = [889696918]
+
+shop_keyboard = types.InlineKeyboardMarkup()
+
+key_click_plus1 = types.InlineKeyboardButton(text="Click upgrade +1 (500)", callback_data="shop_click_plus 1-500")
+key_click_plus2 = types.InlineKeyboardButton(text="Click upgrade +2 (750)", callback_data="shop_click_plus 2-750")
+key_click_plus5 = types.InlineKeyboardButton(text="Click upgrade +5 (2000)", callback_data="shop_click_plus 5-2000")
+key_click_plus10 = types.InlineKeyboardButton(text="Click upgrade +10 (4000)", callback_data="shop_click_plus 10-4000")
+key_click_plus100 = types.InlineKeyboardButton(text="Click upgrade +100 (35000)", callback_data="shop_click_plus 100-35000")
+
+shop_keyboard.add(key_click_plus1)
+shop_keyboard.add(key_click_plus2)
+shop_keyboard.add(key_click_plus5)
+shop_keyboard.add(key_click_plus10)
+shop_keyboard.add(key_click_plus100)
 
 print('ClickBot started')
 
@@ -26,57 +41,47 @@ def start_message(message):
 	if user_balance == None:
 		cursor.execute(f"INSERT INTO users VALUES ({message.from_user.id}, 0)")
 		db.commit()
-		bot.send_message(message.from_user.id, 'Вы зарегистрированы успешно')
+		bot.send_message(message.from_user.id, 'You have registered')
 
 @bot.message_handler(content_types=['text'])
 def text_message(message):
-	try:
-		db = sqlite3.connect('data.db')
-		cursor = db.cursor()
-		user_balance = None
+	print(message.from_user.username)
+	global shop_keyboard
+	db = sqlite3.connect('data.db')
+	cursor = db.cursor()
+	user_balance = None
 
-		for i in cursor.execute(f"SELECT balance FROM users WHERE user_id = {message.from_user.id}"):
-			user_balance = i[0]
+	for i in cursor.execute(f"SELECT balance FROM users WHERE user_id = {message.from_user.id}"):
+		user_balance = i[0]
 
-		for i in cursor.execute(f"SELECT click FROM users WHERE user_id = {message.from_user.id}"):
-			click = i[0]
+	for i in cursor.execute(f"SELECT click FROM users WHERE user_id = {message.from_user.id}"):
+		click = i[0]
 
-		if user_balance != None:
-			if message.text == 'Click':
-				cursor.execute(f"UPDATE users SET balance = {user_balance + click} WHERE user_id = {message.from_user.id}")
-				db.commit()
+	if user_balance != None:
+		if message.text == 'Click':
+			cursor.execute(f"UPDATE users SET balance = {user_balance + click} WHERE user_id = {message.from_user.id}")
+			db.commit()
 
-				bot.delete_message(message.from_user.id, message.message_id)
+			bot.delete_message(message.from_user.id, message.message_id)
 
-			elif message.text == 'Shop':
-				shop_keyboard = types.InlineKeyboardMarkup()
+		elif message.text == 'Shop':
 
-				key_click_plus1 = types.InlineKeyboardButton(text="Click upgrade +1", callback_data="shop_click_plus1")
-				key_click_plus2 = types.InlineKeyboardButton(text="Click upgrade +2", callback_data="shop_click_plus2")
-				key_click_plus5 = types.InlineKeyboardButton(text="Click upgrade +5", callback_data="shop_click_plus5")
-				key_click_plus10 = types.InlineKeyboardButton(text="Click upgrade +10", callback_data="shop_click_plus10")
-				key_click_plus100 = types.InlineKeyboardButton(text="Click upgrade +100", callback_data="shop_click_plus100")
+			bot.send_message(message.from_user.id, f'Shop\nCoins: {user_balance}\nCoins per click: {click}', reply_markup=shop_keyboard)
 
-				shop_keyboard.add(key_click_plus1)
-				shop_keyboard.add(key_click_plus1)
-				shop_keyboard.add(key_click_plus1)
-				shop_keyboard.add(key_click_plus1)
-				shop_keyboard.add(key_click_plus1)
+	else:
+		bot.send_message(message.from_user.id, "You aren't registered, write /start")
 
-				bot.send_message(message.from_user.id, f'МАГАЗИН\nВаш баланс: {user_balance}', reply_markup=shop_keyboard)
-
-		else:
-			bot.send_message(message.from_user.id, 'Вы не зарегистрированы, напишите команду /start')
-
-		user_balance = None
-	except Exception as e:
-		print(e)
+	user_balance = None
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_answer(call):
+	global shop_keyboard
 	if call.data[0:15] == "shop_click_plus":
 		db = sqlite3.connect('data.db')
 		cursor = db.cursor()
+
+		call_data = (call.data).split(' ')
+		call_data = call_data[1].split('-')
 
 		for i in cursor.execute(f"SELECT click FROM users WHERE user_id = {call.from_user.id}"):
 			click = i[0]
@@ -84,14 +89,39 @@ def callback_answer(call):
 		for i in cursor.execute(f"SELECT balance FROM users WHERE user_id = {call.from_user.id}"):
 			balance = i[0]
 
-		if int(call.data[15]) == 1 and balance >= 100:
-			cursor.execute(f'UPDATE users SET click = {click + int(call.data[15])} WHERE user_id = {call.from_user.id}')
-			cursor.execute(f'UPDATE users SET balance = {balance - 100} WHERE user_id = {call.from_user.id}')
-			bot.edit_message_text(f'МАГАЗИН\nВаш баланс: {balance - 100}', chat_id=call.from_user.id, message_id=call.message.id)
+		# if int(call.data[15]) == 1 and balance >= 500:
+		# 	cursor.execute(f'UPDATE users SET click = {click + int(call.data[15])} WHERE user_id = {call.from_user.id}')
+		# 	cursor.execute(f'UPDATE users SET balance = {balance - 500} WHERE user_id = {call.from_user.id}')
+		# 	bot.edit_message_text(f'Shop\nCoins: {balance - 500}\nCoins per click: {click + 1}', chat_id=call.from_user.id, message_id=call.message.id)
+		
+		# if int(call.data[15]) == 2 and balance >= 750:
+		# 	cursor.execute(f'UPDATE users SET click = {click + int(call.data[15])} WHERE user_id = {call.from_user.id}')
+		# 	cursor.execute(f'UPDATE users SET balance = {balance - 750} WHERE user_id = {call.from_user.id}')
+		# 	bot.edit_message_text(f'Shop\nCoins: {balance - 750}\nCoins per click: {click}', chat_id=call.from_user.id, message_id=call.message.id)
+		
+		# if int(call.data[15]) == 5 and balance >= 2000:
+		# 	cursor.execute(f'UPDATE users SET click = {click + int(call.data[15])} WHERE user_id = {call.from_user.id}')
+		# 	cursor.execute(f'UPDATE users SET balance = {balance - 2000} WHERE user_id = {call.from_user.id}')
+		# 	bot.edit_message_text(f'Shop\nCoins: {balance}\nCoins per click: {click}', chat_id=call.from_user.id, message_id=call.message.id)
+		
+		# if call.data[16:17] == '10' and balance >= 4000:
+		# 	cursor.execute(f'UPDATE users SET click = {click + int(call.data[16:17])} WHERE user_id = {call.from_user.id}')
+		# 	cursor.execute(f'UPDATE users SET balance = {balance - 4000} WHERE user_id = {call.from_user.id}')
+		# 	bot.edit_message_text(f'Shop\nCoins: {balance}\nCoins per click: {click}', chat_id=call.from_user.id, message_id=call.message.id)
+		
+		# if call.data[16:18] == '100' and balance >= 35000:
+		# 	cursor.execute(f'UPDATE users SET click = {click + int(call.data[16:18])} WHERE user_id = {call.from_user.id}')
+		# 	cursor.execute(f'UPDATE users SET balance = {balance - 35000} WHERE user_id = {call.from_user.id}')
+		# 	bot.edit_message_text(f'Shop\nCoins: {balance}\nCoins per click: {click}', chat_id=call.from_user.id, message_id=call.message.id)
+
+
+		cursor.execute(f'UPDATE users SET click = {click + int(call_data[0])} WHERE user_id = {call.from_user.id}')
+		cursor.execute(f'UPDATE users SET balance = {balance - int(call_data[1])} WHERE user_id = {call.from_user.id}')
+		bot.edit_message_text(f'Shop\nCoins: {balance - int(call_data[1])}\nCoins per click: {click + int(call_data[0])}', chat_id=call.from_user.id, message_id=call.message.id)
 
 		db.commit()
 
-# try:
-bot.polling()
-# except Exception as e:
-# 	print(e)
+try:
+	bot.polling()
+except Exception as e:
+	print(e)
