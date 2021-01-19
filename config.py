3d1aa75370fef16301a81df611dc96726debd7ab
Timo <66961498+nuokey@users.noreply.github.com>
2021-01-20 00:14:50 +0300

    Update

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-token = '1413603655:AAHGSnGRJSpcc-trU9q-zZiLqfXmaLrwFpg'
+token = '1453258217:AAEtKG4b10UYW582oBNxr3dM-Ct43X_h7q4'
+
+click_upgrade1 = 1000
+click_upgrade5 = 4000
+click_upgrade10 = 6500
+click_upgrade100 = 50000
+click_upgrade1000 = 90000
